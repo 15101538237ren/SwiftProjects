7f3f0a8e7796b430b3cc4ef33f301a734dbc17fd
Honglei <hongleir@uci.edu>
2020-04-26 23:51:14 -0700

    succuess implemented recurrent cards

--- shuaci/shuaci/Controllers/MainScreenViewController.swift
+++ shuaci/shuaci/Controllers/MainScreenViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 import LeanCloud
 
 class MainScreenViewController: UIViewController {
-    @IBOutlet var card: CardUIView!
+    @IBOutlet var mainScreenUIView: MainScreenUIView!
+    @IBOutlet var cards: [CardUIView]!
+    var currentIndex:Int = 0
+    var words: [Word] = [
+        Word(wordHead: "sea", trans: [["tranCn":"n. 海；海洋", "pos": "n"]], usphone: "siː", ukphone: "siː", usspeech: "sea_0", ukspeech: "sea_1", remMethod: "", relWords: [], phrases: [["pContent":"china sea", "pCn": "中国海"]], synoWords: [["pos": ["n"], "tran":["n. [海洋][地理]海；海洋；许多；大量"], "hwds":["ocean", "lots of", "wealth"]]], sentences: [["sContent": "Sea and sky seemed to blend.", "sCn": "大海和蓝天似乎相融合。"]]),
+        Word(wordHead: "beach", trans: [["tranCn":"海滩，沙滩", "tranOther":"an area of sand or small stones at the edge of the sea or a lake", "pos":"n"]], usphone: "bitʃ", ukphone: "biːtʃ", usspeech: "beach_0", ukspeech: "beach_1", remMethod: "pharma (药) + cy (学科) → 药剂学", relWords: [], phrases:  [["pContent":"china sea", "pCn": "中国海"]], synoWords: [["pos": ["n"], "tran":["n. [海洋][地理]海；海洋；许多；大量"], "hwds":["ocean", "lots of", "wealth"]]], sentences: [["sContent": "Sea and sky seemed to blend.", "sCn": "大海和蓝天似乎相融合。"]]),
+        Word(wordHead: "Paris", trans: [["tranCn":"n.巴黎","pos":"n"]], usphone: "'pærɪs", ukphone: "'pærɪs", usspeech: "Paris_0", ukspeech: "Paris_1", remMethod: "pharma (药) + cy (学科) → 药剂学", relWords: [], phrases:  [["pContent":"china sea", "pCn": "中国海"]], synoWords: [["pos": ["n"], "tran":["n. [海洋][地理]海；海洋；许多；大量"], "hwds":["ocean", "lots of", "wealth"]]], sentences: [["sContent": "Sea and sky seemed to blend.", "sCn": "大海和蓝天似乎相融合。"]]),
+        Word(wordHead: "aurora", trans: [["tranCn":"[地物] 极光；曙光","pos":"n"]], usphone: "ɔ:'rɔ:rə", ukphone: "ɔ:'rɔ:rə", usspeech: "aurora_0", ukspeech: "aurora_1", remMethod: "pharma (药) + cy (学科) → 药剂学", relWords: [], phrases:  [["pContent":"china sea", "pCn": "中国海"]], synoWords: [["pos": ["n"], "tran":["n. [海洋][地理]海；海洋；许多；大量"], "hwds":["ocean", "lots of", "wealth"]]], sentences: [["sContent": "Sea and sky seemed to blend.", "sCn": "大海和蓝天似乎相融合。"]]),
+        Word(wordHead: "pharmacy", trans: [["tranCn":"药房；配药学，药剂学；制药业；一批备用药品", "tranOther":"a shop or a part of a shop where medicines are prepared and sold", "pos":"n"]], usphone: "'fɑrməsi", ukphone: "'fɑːməsɪ", usspeech: "pharmacy_0", ukspeech: "pharmacy_1", remMethod: "pharma (药) + cy (学科) → 药剂学", relWords: [], phrases:  [["pContent":"china sea", "pCn": "中国海"]], synoWords: [["pos": ["n"], "tran":["n. [海洋][地理]海；海洋；许多；大量"], "hwds":["ocean", "lots of", "wealth"]]], sentences: [["sContent": "Sea and sky seemed to blend.", "sCn": "大海和蓝天似乎相融合。"]])]
+    
     
     @IBAction func close(segue:UIStoryboardSegue) {
         dismiss(animated: true, completion: nil)
@@ -18,18 +27,44 @@ class MainScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initCards()
+        let card = cards[0]
+        let xshift:CGFloat = card.frame.size.width/8.0
+        card.transform = CGAffineTransform(translationX: -xshift, y:0.0).rotated(by: -xshift*0.61/card.center.x)
     }
-    override func viewDidAppear(_ animated: Bool) {
-//        if let user = LCApplication.default.currentUser {
-//            // 跳到首页
-//            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-//            let mainPanelViewController = storyBoard.instantiateViewController(withIdentifier: "mainPanelViewController") as! MainPanelViewController
-//
-//            DispatchQueue.main.async {
-//                self.present(mainPanelViewController, animated: true, completion: nil)
-//            }
-//        }
+    func initCards() {
+        for index in 0..<cards.count
+        {
+            let card = cards[index]
+            let word = words[index % words.count]
+            card.cardImageView?.image = UIImage(named: word.wordHead)
+            card.wordLabel?.text = word.wordHead
+            card.meaningLabel?.text = word.trans[0]["tranCn"]!
+            card.accentLabel?.text = "美"
+            card.phoneticLabel?.text = word.usphone
+            card.rememberImageView?.image = UIImage(named: "bushou")
+        }
+    }
+    func moveCard() {
+        let card = cards[(currentIndex + 1) % 2]
+        let word = words[(currentIndex + 1) % words.count]
+        
+        card.cardImageView?.image = UIImage(named: word.wordHead)
+        card.wordLabel?.text = word.wordHead
+        card.meaningLabel?.text = word.trans[0]["tranCn"]!
+        card.accentLabel?.text = "美"
+        card.phoneticLabel?.text = word.usphone
+        card.rememberImageView?.image = UIImage(named: "bushou")
+        card.rememberImageView?.alpha = 0
+        card.transform = .identity
+        card.alpha = 1
+        card.center = CGPoint(x: view.center.x, y: view.center.y)
+        card.layer.zPosition = 20
+        mainScreenUIView.bringSubviewToFront(cards[currentIndex % 2])
+        mainScreenUIView.sendSubviewToBack(card)
+//        let card1 = cards[1]
+//        card1.layer.zPosition = 0
+        //cards.append(card)
     }
     
     @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
@@ -45,13 +80,13 @@ class MainScreenViewController: UIViewController {
         card.transform = CGAffineTransform(rotationAngle: 0.61 * xFromCenter / view.center.x).scaledBy(x: scale, y: scale)
         if xFromCenter > 0
         {
-            card.rememberImageView.image = UIImage(named: "bushou")
+            card.rememberImageView?.image = UIImage(named: "bushou")
         }
         else
         {
-            card.rememberImageView.image = UIImage(named: "huile")
+            card.rememberImageView?.image = UIImage(named: "huile")
         }
-        card.rememberImageView.alpha = alpha
+        card.rememberImageView?.alpha = alpha
         
         
         if sender.state == UIGestureRecognizer.State.ended
@@ -63,6 +98,8 @@ class MainScreenViewController: UIViewController {
                     card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 75)
                     card.alpha = 0
                 })
+                self.currentIndex += 1
+                self.moveCard()
                 return
             }
             else if card.center.x > (0.75 * view.frame.width)
@@ -72,24 +109,21 @@ class MainScreenViewController: UIViewController {
                     card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 75)
                     card.alpha = 0
                 })
+                self.currentIndex += 1
+                self.moveCard()
                 return
             }
-            resetCard()
+            resetCard(card: card)
         }
     }
-    
-    @IBAction func reset(_ sender: UIButton) {
-        resetCard()
-    }
-    
-    func resetCard()
+    func resetCard(card: CardUIView)
     {
         UIView.animate(withDuration: 0.2, animations:
         {
-            self.card.center = self.view.center
-            self.card.alpha = 1
+            card.center = self.view.center
+            card.alpha = 1
         })
-        self.card.rememberImageView.alpha = 0
+        card.rememberImageView?.alpha = 0
         card.transform = .identity
     }
 }
--- shuaci/shuaci/Views/CardUIView.swift
+++ shuaci/shuaci/Views/CardUIView.swift
@@ -9,21 +9,34 @@
 import UIKit
 
 class CardUIView: UIView {
-    @IBOutlet var cardImageView: UIImageView!{
+    @IBOutlet var cardImageView: UIImageView?{
         didSet {
-            cardImageView.layer.cornerRadius = 20.0
-            cardImageView.layer.masksToBounds = true
+            cardImageView?.layer.cornerRadius = 20.0
+            cardImageView?.layer.masksToBounds = true
         }
     }
-    @IBOutlet var wordLabel: UILabel!
+    @IBOutlet var wordLabel: UILabel?
     
-    @IBOutlet var meaningLabel: UILabel!{
+    @IBOutlet var meaningLabel: UILabel?{
         didSet {
-            meaningLabel.numberOfLines = 0
+            meaningLabel?.numberOfLines = 0
         }
     }
-    @IBOutlet var rememberImageView: UIImageView!
-    @IBOutlet var accentLabel: UILabel!
-    @IBOutlet var phoneticLabel: UILabel!
-    @IBOutlet var audioButton: UIButton!
+    @IBOutlet var accentLabel: UILabel?
+    @IBOutlet var phoneticLabel: UILabel?
+    @IBOutlet var rememberImageView: UIImageView?
+    
+    init(cardImage: String, word: String, meaning: String, accent:String, phone: String){
+        super.init(frame: CGRect(x: 0, y: 0, width: 296, height: 420))
+        self.cardImageView?.image = UIImage(named: cardImage)
+        self.wordLabel?.text = word
+        self.meaningLabel?.text = meaning
+        self.accentLabel?.text = accent
+        self.phoneticLabel?.text = phone
+        self.rememberImageView?.image = UIImage(named: "bushou")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 }
