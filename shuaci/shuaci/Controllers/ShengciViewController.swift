0b95271ab8f705e8214de64c108f627fefada8f6
Honglei <hongleir@uci.edu>
2020-05-01 21:26:10 -0700

    hide the hide back button in navigation item

--- shuaci/shuaci/Controllers/ShengciViewController.swift
+++ shuaci/shuaci/Controllers/ShengciViewController.swift
@@ -12,10 +12,16 @@ class ShengciViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.hidesBackButton = true 
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // remove left buttons (in case you added some)
+         self.navigationItem.leftBarButtonItems = []
+        // hide the default back buttons
+         self.navigationItem.hidesBackButton = true
+    }
 
     /*
     // MARK: - Navigation
