fa31896240b10bb2b53d9695aaafd74def47ce68
Honglei <hongleir@uci.edu>
2020-09-01 11:10:00 -0700

    local

--- shuaci/shuaci/Controllers/SetMemOptionViewController.swift
+++ shuaci/shuaci/Controllers/SetMemOptionViewController.swift
@@ -23,6 +23,8 @@ class SetMemOptionViewController: UIViewController, UIPickerViewDelegate, UIPick
     
     @IBOutlet weak var dailyNumWordPickerView: UIPickerView!
     
+    var viewTranslation = CGPoint(x: 0, y: 0)
+    
     let number_of_words: [Int] = [10, 20, 30, 40, 50, 100, 150, 200, 300]
     
     @IBAction func unwind(sender: UIButton) {
@@ -48,6 +50,29 @@ class SetMemOptionViewController: UIViewController, UIPickerViewDelegate, UIPick
         dismiss(animated: true, completion: nil)
     }
     
+    @objc func handleSlideDismiss(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .changed:
+            viewTranslation = sender.translation(in: view)
+            if viewTranslation.y > 0 {
+                UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                    self.view.transform = CGAffineTransform(translationX: 0, y: self.viewTranslation.y)
+                })
+            }
+        case .ended:
+            if viewTranslation.y < 200 {
+                UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                    self.view.transform = .identity
+                })
+            } else {
+                dismiss(animated: true, completion: nil)
+            }
+        default:
+            break
+        }
+    }
+    
+    
     func addBlurBackgroundView(){
         let blurEffect = getBlurEffect()
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
@@ -68,6 +93,9 @@ class SetMemOptionViewController: UIViewController, UIPickerViewDelegate, UIPick
         view.backgroundColor = .clear
         addBlurBackgroundView()
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
+        
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleSlideDismiss(sender:))))
+        
         view.isUserInteractionEnabled = true
         
         backBtn.theme_tintColor = "Global.backBtnTintColor"
