d021dd265193b20e2fde21fccc78acf66b55922c
Honglei Ren <hongleir@uci.edu>
2020-10-03 17:16:50 -0700

    finish main screen

--- Blackpink/Blackpink/Controllers/MainViewController.swift
+++ Blackpink/Blackpink/Controllers/MainViewController.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 import CloudKit
+import UILoadControl
 
-class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate {
     // Outlet Variables
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet var soloBtns: [UIButton]!{
+        didSet{
+            for soloBtn in soloBtns{
+                soloBtn.layer.cornerRadius = soloBtn.layer.frame.width/2.0
+                soloBtn.layer.masksToBounds = true
+            }
+        }
+    }
     
     // Variables
     var spinner = UIActivityIndicatorView()
@@ -18,27 +27,32 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
     private var imageCache = NSCache<CKRecord.ID, NSURL>()
     var sortType:SortType = .byLike
     var refreshControl: UIRefreshControl?
-    
+    var queryCursor: CKQueryOperation.Cursor? = nil
+    var loaded: Bool = false
     // Constants
     
     let cellPadding:CGFloat = 1.5
     let numberOfItemsPerRow:CGFloat = 3
     let currentWallpaperCategory:WallpaperCategory = .Group
+    let resultLimit:Int = 27
+    
     // Functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
         collectionView.delegate = self
+        collectionView.loadControl = UILoadControl(target: self, action: #selector(load))
+        collectionView.loadControl?.heightLimit = 100.0 //The default is 80.0
         initSpinnerAndRefreshControl()
-        fetchRecordsFromCloud()
+        load()
     }
     
     func initSpinnerAndRefreshControl() {
         refreshControl = UIRefreshControl()
         refreshControl?.backgroundColor = .white
         refreshControl?.tintColor = .gray
-        refreshControl?.addTarget(self, action: #selector(fetchRecordsFromCloud), for: UIControl.Event.valueChanged)
+        refreshControl?.addTarget(self, action: #selector(load), for: UIControl.Event.valueChanged)
         spinner.style = .medium
         spinner.hidesWhenStopped = true
         view.addSubview(spinner)
@@ -55,15 +69,37 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
             return NSSortDescriptor(key: "modificationDate", ascending: false)
         case .byLike:
             return NSSortDescriptor(key: "likes", ascending: false)
-        default:
-            return NSSortDescriptor(key: "modificationDate", ascending: false)
         }
     }
     
-    @objc func fetchRecordsFromCloud(){
-        wallpapers.removeAll()
-        collectionView.reloadData()
+    func completionHandlerAfterLoad(error: Error?, cursor: CKQueryOperation.Cursor?) -> Void{
+        if error != nil{
+            print("Error in load Wallpapers: \(error?.localizedDescription ?? "")")
+        } else{
+            loaded = true
+            if let cursor = cursor {
+                queryCursor = cursor
+            }
+            DispatchQueue.main.async {
+                self.spinner.stopAnimating()
+                self.collectionView.reloadData()
+                self.collectionView.loadControl?.endLoading()
+                if let refreshControl = self.refreshControl {
+                    if refreshControl.isRefreshing{
+                        refreshControl.endRefreshing()
+                    }
+                }
+            }
+        }
         
+    }
+    
+    @objc func load(){
+        if loaded && queryCursor == nil{
+            self.completionHandlerAfterLoad(error: nil, cursor: nil)
+            return
+        }
+        collectionView.reloadData()
         //Fetch data using Convienence API
         let cloudContainer = CKContainer.init(identifier: icloudContainerID)
         let publicDatabase = cloudContainer.publicCloudDatabase
@@ -72,34 +108,24 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
         let query = CKQuery(recordType: "Wallpaper", predicate: predicate)
         let sortDescriptor = getSortDescriptor(sortType: sortType)
         query.sortDescriptors = [sortDescriptor]
+        print(queryCursor==nil)
+        let queryOperation = queryCursor == nil ? CKQueryOperation(query: query): CKQueryOperation(cursor: queryCursor!)
         
-        let queryOperation = CKQueryOperation(query: query)
-            queryOperation.desiredKeys = ["image", "likes"]
-            queryOperation.queuePriority = .veryHigh
-            queryOperation.resultsLimit = 9 * 3
-            queryOperation.recordFetchedBlock = { (record) -> Void in
-                self.wallpapers.append(record)
-            }
-
-            queryOperation.queryCompletionBlock = { [unowned self] (cursor, error) -> Void in
-                if let error = error {
-                    print("Failed to get data from iCloud - \(error.localizedDescription)")
-                    return
-                }
-                
-                print("Successfully retrieve the data from iCloud")
-                print("#wallpapers: \(wallpapers.count)")
-                DispatchQueue.main.async {
-                    self.spinner.stopAnimating()
-                    self.collectionView.reloadData()
-                    if let refreshControl = self.refreshControl {
-                        if refreshControl.isRefreshing{
-                            refreshControl.endRefreshing()
-                        }
-                    }
-                }
-            }
-            publicDatabase.add(queryOperation)
+        queryOperation.desiredKeys = ["likes"]
+        queryOperation.queuePriority = .veryHigh
+        queryOperation.resultsLimit = resultLimit
+        queryOperation.recordFetchedBlock = { (record) -> Void in
+            self.wallpapers.append(record)
+        }
+        queryOperation.queryCompletionBlock = {(cursor, error) -> Void in
+            self.completionHandlerAfterLoad(error: error, cursor: cursor)
+        }
+        publicDatabase.add(queryOperation)
+    }
+    
+    //update loadControl when user scrolls de tableView
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        scrollView.loadControl?.update()
     }
     
     func likesToString(likes: Int) -> String{
@@ -157,6 +183,7 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
                                 cell.setNeedsLayout()
                             }
                             self.imageCache.setObject(imageAsset.fileURL! as NSURL, forKey: wallpaper.recordID)
+                            print("Loaded image for cell \(indexPath.row)")
                         }
                     }
                 }
@@ -170,9 +197,6 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
         let width = (collectionView.frame.size.width -  (numberOfItemsPerRow + 1) * cellPadding) / numberOfItemsPerRow
         
         let height = (collectionView.frame.size.height - (numberOfItemsPerRow + 1) * cellPadding) / 3.0
-        print("width: \(width), height: \(height)")
-        cell.contentView.setNeedsLayout()
-        cell.contentView.layoutIfNeeded()
         return CGSize(width: width, height: height)
     }
     
---
+++ Blackpink/Pods/Target Support Files/Pods-Blackpink-BlackpinkUITests/Pods-Blackpink-BlackpinkUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Blackpink_BlackpinkUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Blackpink_BlackpinkUITests
+@end
